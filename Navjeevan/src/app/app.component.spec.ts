f593945dacd792635c63251dc6da7551953f53ad
Nik-09 <nikhil.agarwal.2019@gmail.com>
2021-05-31 23:20:06 +0530

    refactoring navjeevan

--- Navjeevan/src/app/app.component.spec.ts
+++ Navjeevan/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'navjeevan'`, () => {
+  it(`should have as title 'Navjeevan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('navjeevan');
+    expect(app.title).toEqual('Navjeevan');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('navjeevan app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Navjeevan app is running!');
   });
 });
