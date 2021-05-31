40e2e0534fa36e42fe7567d5c9c6bfe5a8133bb5
Nik-09 <nikhil.agarwal.2019@gmail.com>
2021-05-31 23:41:30 +0530

    Adds about page content

--- Navjeevan/src/app/app.component.ts
+++ Navjeevan/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @Component({
   selector: 'app-root',
--- Navjeevan/src/app/app.module.ts
+++ Navjeevan/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule
